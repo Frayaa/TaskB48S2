a9f9d8f20c3ba2f2e334010ac95313a2c591745a
Frayaa <fitrah.fraya@gmail.com>
2023-08-08 07:33:10 +0700

    done

---
+++ threads-fe/src/features/sidebar/SideBar.tsx
@@ -0,0 +1,123 @@
+// import {
+//   Button,
+//   ButtonGroup,
+//   Container,
+//   Heading,
+//   Stack,
+//   VStack,
+// } from "@chakra-ui/react"
+// import { NavLink } from "react-router-dom"
+
+// const SideBar = () => {
+//   return (
+//     <>
+//       <Container width="300px" height="max-content" backgroundColor="blue">
+//         <Heading>Circle</Heading>
+//         <VStack float="left">
+//           <ButtonGroup flexDirection="column" gap="5" marginTop="20px">
+//             <NavLink
+//               to="/"
+//               //   style={({ isActive }) => ({
+//               //     backgroundColor: isActive ? "blue" : "grey",
+//               //   })}
+//             >
+//               <Button>Home</Button>
+//             </NavLink>
+
+//             <NavLink
+//               to="/search"
+//               //   style={({ isActive }) => ({
+//               //     backgroundColor: isActive ? "blue" : "grey",
+//               //   })}
+//             >
+//               <Button>Search</Button>
+//             </NavLink>
+
+//             <NavLink
+//               to="/follow"
+//               //   style={({ isActive }) => ({
+//               //     backgroundColor: isActive ? "blue" : "grey",
+//               //   })}
+//             >
+//               <Button>Follows</Button>
+//             </NavLink>
+
+//             <NavLink
+//               to="/profile"
+//               //   style={({ isActive }) => ({
+//               //     backgroundColor: isActive ? "blue" : "grey",
+//               //   })}
+//             >
+//               <Button>Profile</Button>
+//             </NavLink>
+//           </ButtonGroup>
+//         </VStack>
+//       </Container>
+//     </>
+//   )
+// }
+
+// export default SideBar
+
+// import {
+//   Box,
+//   Button,
+//   Drawer,
+//   DrawerOverlay,
+//   DrawerCloseButton,
+//   DrawerHeader,
+//   DrawerBody,
+//   DrawerContent,
+//   VStack,
+// } from '@chakra-ui/react'
+
+// interface SideBar {
+//   onClose: Function
+//   isOpen: boolean
+//   variant: 'drawer' | 'sidebar'
+// }
+
+// const SidebarContent = ({ onClick }: { onClick: Function }) => (
+//   <VStack>
+//     <Button onClick={onClick} w="100%">
+//       Home
+//     </Button>
+//     <Button onClick={onClick} w="100%">
+//       About
+//     </Button>
+//     <Button onClick={onClick} w="100%">
+//       Contact
+//     </Button>
+//   </VStack>
+// )
+
+// const Sidebar = ({ isOpen, variant, onClose }: Props) => {
+//   return variant === 'sidebar' ? (
+//     <Box
+//       position="fixed"
+//       left={0}
+//       p={5}
+//       w="200px"
+//       top={0}
+//       h="100%"
+//       bg="#dfdfdf"
+//     >
+//       <SidebarContent onClick={onClose} />
+//     </Box>
+//   ) : (
+//     <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+//       <DrawerOverlay>
+//         <DrawerContent>
+//           <DrawerCloseButton />
+//           <DrawerHeader>Chakra-UI</DrawerHeader>
+//           <DrawerBody>
+//             <SidebarContent onClick={onClose} />
+//           </DrawerBody>
+//         </DrawerContent>
+//       </DrawerOverlay>
+//     </Drawer>
+//   )
+// }
+
+// export default Sidebar
+
--- threads-fe/src/features/thread/component/ThreadCrad.tsx
+++ threads-fe/src/features/thread/component/ThreadCrad.tsx
@@ -1,15 +1,19 @@
 import {
   Box,
   Button,
+  Container,
   Flex,
   Grid,
   HStack,
+  Heading,
   Image,
   Stack,
   Text,
+  VStack,
 } from "@chakra-ui/react"
 import { FcLikePlaceholder } from "react-icons/fc"
 import { BiMessageDots } from "react-icons/bi"
+// import SideBar from "@/features/sidebar/SideBar"
 
 interface ThreadCard {
   //mirip struct golang
@@ -28,42 +32,65 @@ interface ThreadCard {
 export const ThreadCard = (props: ThreadCard) => {
   return (
     <>
-      <Flex padding="40px">
-        <Image
-          boxSize="80px"
-          borderRadius="full"
-          width="260px"
-          objectFit="cover"
-          src={props.author_picture}
-        />
-        <Grid marginLeft="20px" marginTop="20px">
-          <Flex>
-            <Text fontWeight="bold">{props.author_full_name}</Text>
-            <Text marginLeft="10px">@{props.author_username}</Text>
-            <Text marginLeft="10px">{props.posted_at}</Text>
-          </Flex>
-          <Text noOfLines={[1, 2, 3]}>{props.content}</Text>
+      <Container>
+        {/* <Flex> */}
+        {/* <VStack>{SideBar()}</VStack> */}
 
+        {/* <VStack> */}
+        {/* <Heading>Home</Heading> */}
+        {/* <Box>
+              <Image
+                boxSize="80px"
+                borderRadius="full"
+                width="260px"
+                objectFit="cover"
+                src={props.author_picture}
+              />
+              What's Hapenning?
+            </Box> */}
+        <Flex padding="40px">
           <Image
-            src={props.image}
-            width="300px"
-            height="400px"
-            marginTop="15px"
+            // boxSize="50px"
+            height="60px"
+            borderRadius="50%"
+            width="600px"
+            objectFit="cover"
+            marginTop="25px"
+            src={props.author_picture}
           />
-          <Box style={{ marginTop: "20px" }}>
-            <Button
-              style={{ backgroundColor: props.is_liked ? "red" : "#fcfcfc" }}
-            >
-              <FcLikePlaceholder />
-              <Text textAlign="justify">{props.likes_count}</Text>
-            </Button>
-            <Button width="150px" marginLeft="10px" backgroundColor="#fcfcfc">
-              <BiMessageDots />
-              <Text marginLeft="5px">{props.replies_count} Replies</Text>
-            </Button>
-          </Box>
-        </Grid>
-      </Flex>
+          <Grid marginLeft="20px" marginTop="20px">
+            <Flex>
+              <Text fontWeight="bold">{props.author_full_name}</Text>
+              <Text marginLeft="10px">@{props.author_username}</Text>
+              <Text marginLeft="10px">{props.posted_at}</Text>
+            </Flex>
+            <Text noOfLines={[1, 2, 3]}>{props.content}</Text>
+
+            <Image
+              src={props.image}
+              width="300px"
+              height="400px"
+              marginTop="15px"
+            />
+            <Box style={{ marginTop: "20px" }}>
+              <Button
+                style={{
+                  backgroundColor: props.is_liked ? "red" : "#fcfcfc",
+                }}
+              >
+                <FcLikePlaceholder />
+                <Text textAlign="justify">{props.likes_count}</Text>
+              </Button>
+              <Button width="150px" marginLeft="10px" backgroundColor="#fcfcfc">
+                <BiMessageDots />
+                <Text marginLeft="5px">{props.replies_count} Replies</Text>
+              </Button>
+            </Box>
+          </Grid>
+        </Flex>
+        {/* </VStack> */}
+        {/* </Flex> */}
+      </Container>
     </>
   )
 }
